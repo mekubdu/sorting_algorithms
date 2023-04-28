095343bdd837d69d4714dbdb5499b44b314d4db7
meku <mekubdu@gmail.com>
2023-04-28 04:11:50 -0700

    updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,34 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * bubble_sort - Sorts Integers's array in ascending order
+ * order using the Bubble sort
+ * @array: array of integers
+ * @size: size of array
+ * Return: sorted array
  */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
- */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	int aux = 0, sw = 1;
+	unsigned int j = 0;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (sw != 0)
 	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+		sw = 0;
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				sw = 1;
 				print_array(array, size);
-				bubbly = false;
 			}
 		}
-		len--;
 	}
 }
